e24ae3d8b3b4db4c8433e5918ce61151bef00999
Trh2023 <trh20231@gmail.com>
2026-09-13 20:45:34 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "app:app",          # مسیر ماژول، نه آبجکت app
+        host="0.0.0.0",
+        port=port,
+        log_level="info",
+    )
